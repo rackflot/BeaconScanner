1cb975289d5025a608071b66b7f2b4e7e93f4afb
rackflot <rackflot@gmail.com>
2020-03-22 18:02:56 -0400

    fresh git

---
+++ ParseReturn.py
@@ -0,0 +1,6 @@
+import blescan
+import sys
+
+import bluetooth._bluetooth as bluez
+
+str iResult = 'ac:23:3f:a2:53:89, b9407f30f5f8466eaff925556b57fe6d, 0064, 0065, e8'
---
+++ testblescan.py
@@ -0,0 +1,38 @@
+# test BLE Scanning software
+# jcs 6/8/2014
+
+import blescan
+import sys
+
+import bluetooth._bluetooth as bluez
+
+import ptvsd
+
+dev_id = 0
+
+# Allow other computers to attach to ptvsd at this IP address and port.
+# ptvsd.enable_attach(address=('192.168.1.175', 3000), redirect_output=True)
+
+# Pause the program until a remote debugger is attached
+# ptvsd.wait_for_attach()
+
+sock = bluez.hci_open_dev(dev_id)
+print ("ble thread started")
+
+blescan.hci_le_set_scan_parameters(sock)
+blescan.hci_enable_le_scan(sock)
+
+x = 0
+# str sBeacon 
+while True:
+    returnedList = blescan.parse_events(sock, 10)
+    try:
+        returnedList.sort()
+        # x = returnedList.index("b940")
+        break
+    except ValueError:
+        print("wtf")
+    print ("----------")
+    for beacon in returnedList:
+        print (beacon)
+
